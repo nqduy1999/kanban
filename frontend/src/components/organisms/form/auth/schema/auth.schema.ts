febc3c8e5c280a2a74fcfbad46508a14b2902099
Super intern dev <duy.se.it99@gmail.com>
2022-10-07 16:39:47 +0700

    chore: update schema validate

--- frontend/src/components/organisms/form/auth/schema/auth.schema.ts
+++ frontend/src/components/organisms/form/auth/schema/auth.schema.ts
@@ -1,6 +1,6 @@
-import { REQUIRED_ERR } from "@/commons/form";
+import { MAX_ERR, MIN_ERR, REQUIRED_ERR } from "@/commons/form";
 import * as Yup from "yup";
 
-export const passwordValidationSchema = Yup.string().required(REQUIRED_ERR("password"));
-export const confirmPasswordValidationSchema = Yup.string().required(REQUIRED_ERR("confirm password"));
-export const usernameValidationSchema = Yup.string().required(REQUIRED_ERR("username"));
+export const passwordValidationSchema = Yup.string().min(8, MIN_ERR(8)).max(32, MAX_ERR(32)).required(REQUIRED_ERR("password"));
+export const confirmPasswordValidationSchema = Yup.string().min(8, MIN_ERR(8)).max(32, MAX_ERR(32)).required(REQUIRED_ERR("confirm password"));
+export const usernameValidationSchema = Yup.string().min(8, MIN_ERR(8)).max(32, MAX_ERR(32)).required(REQUIRED_ERR("username"));
