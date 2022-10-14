24a6c7f255289422eaed023154fb354309a7496c
Super intern dev <duy.se.it99@gmail.com>
2022-10-15 00:21:28 +0700

    chore: remove old backend

--- backend/src/server.ts
+++
@@ -1,9 +0,0 @@
-import app from "./app";
-const PORT = process.env.PORT || 2222;
-// Database
-import './configs/database'
-
-app.listen(PORT, () => {
-  console.log('Express server listening on port ' + PORT);
-})
-
--- backend/src/v1/routes/index.ts
+++
@@ -1,5 +0,0 @@
-export * from '../modules/auth/routes'
-export * from '../modules/tasks/routes'
-export * from '../modules/section/routes'
-export * from '../modules/board/routes'
-export * from '../modules/admin/routes'
