69cbd6a172c86a55861018bf06831c3db5e08ad5
Super intern dev <duy.se.it99@gmail.com>
2022-10-15 09:08:50 +0700

    feat(auth): update authen

--- frontend/src/components/templates/auth-layout/auth-layout.template.tsx
+++ frontend/src/components/templates/auth-layout/auth-layout.template.tsx
@@ -1,5 +1,6 @@
-import { Header, Meta } from "@/components/organisms";
-import { ReactNode } from "react";
+import { Header, Meta, NavbarMobile } from "@/components/organisms";
+import React, { ReactNode } from "react";
+import { isMobile } from "react-device-detect";
 
 type ILayoutProps = {
   meta:
@@ -12,11 +13,18 @@ type ILayoutProps = {
 };
 
 const AuthLayout = (props: ILayoutProps) => {
+  const [mounted, setMounted] = React.useState(false);
+
+  React.useEffect(() => {
+    setMounted(true);
+  }, []);
+
   return (
     <div className="bg-white dark:bg-slate-800 h-screen	">
       <Meta title={props.meta.title} description={props.meta.description} />
       <Header />
       {props.children}
+      {mounted && isMobile && <NavbarMobile />}
     </div>
   );
 };
