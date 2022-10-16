738d97608d7f3717f824fc4e72cc95196254d30c
Super intern dev <duy.se.it99@gmail.com>
2022-10-16 23:14:29 +0700

    chore: update background

--- frontend/src/components/organisms/header/header.organism.tsx
+++ frontend/src/components/organisms/header/header.organism.tsx
@@ -17,7 +17,7 @@ const Header = () => {
           className="hidden w-full md:block md:w-auto"
           id="navbar-multi-level"
         >
-          <ul className="flex flex-col bg-gray-200 rounded-lg border border-gray-100 md:flex-row md:space-x-2 md:mt-0 md:text-sm md:font-medium md:border-0 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+          <ul className="flex flex-col bg-gray-200 rounded-lg border border-gray-100 md:flex-row md:space-x-2 md:mt-0 md:text-sm md:font-medium md:border-0 dark:bg-gray-800 dark:border-gray-700">
             <li>
               <div className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
                 <LanguageToogle />
